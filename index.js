f8d3552f8ef0b7dc87b658a1a3192a0943a805d6
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-03-20 11:16:24 -0600

    Create index file with commented structure

---
+++ index.js
@@ -0,0 +1,20 @@
+// import dotenv
+
+// import Client and GatewayIntentBits from discord.js
+
+// call import config()
+
+// create a new discord client instance
+/**
+ * Needed intents
+ * - GatewayIntentBits
+ * Guilds
+ * GuildMessages
+ * MessageContent
+ * It will need to access private messages as well as channel messages and respond when using @botname
+ */
+
+// create event listener for when client is ready
+// log message "Bot is online"
+
+// have client login() using process.env.DISCORD_TOKEN
