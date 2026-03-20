af6899046867df4b6753c83904a5d124dfaaa023
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-03-20 14:20:23 -0600

    Setup discord bot and login

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import dotenv and config
 import { config } from 'dotenv';
 // import Client and GatewayIntentBits from discord.js
-import {GatewayIntentBits, Client} from 'discord.js';
+import {GatewayIntentBits, Client, Events} from 'discord.js';
 
 config();
 
@@ -10,16 +10,14 @@ const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 
 // create a new discord client instance
-/**
- * Needed intents
- * - GatewayIntentBits
- * Guilds
- * GuildMessages
- * MessageContent
- * It will need to access private messages as well as channel messages and respond when using @botname
- */
+const client = new Client({intents:[GatewayIntentBits.Guilds]});
+
 
 // create event listener for when client is ready
 // log message "Bot is online"
+client.once(Events.ClientReady, (readyClient)=>{
+    console.log(`Bot is online as ${readyClient.user.tag}`)
+});
 
 // have client login() using DISCORD_TOKEN
+client.login(DISCORD_TOKEN);
