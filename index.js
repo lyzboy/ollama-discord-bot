b6dde00016c1bd0b0c837078ce9a45618d0cd206
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-03-20 23:12:58 -0600

    Add listener for guild messages
    Added a listener that will listen to all messages sent

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ import {
   Collection,
   MessageFlags,
 } from "discord.js";
+import { createRequire } from "node:module";
 
 config();
+
+const require = createRequire(import.meta.url);
 
 // discord token
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
@@ -36,12 +39,66 @@ client.login(DISCORD_TOKEN);
 
 client.commands = new Collection();
 
+const foldersPath = path.join(import.meta.dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  // change to async?
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // set a new item in the collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`,
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  // how to log the incoming interaction
+  console.log(interaction);
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.log(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: `There was an error while executing this command!`,
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: `There was an error while executing this command!`,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+});
+
 // listen for new messages every time it happens
+client.on(Events.MessageCreate, async (interaction) => {
+  console.log(interaction);
 
-// prevent the infinite loop by inspecting the author of the message and break the loop if the author is a bot.
+  // prevent the infinite loop by inspecting the author of the message and break the loop if the author is a bot.
 
-// condition to check if the bot was "mentioned" in the message and proceed if it was
+  // condition to check if the bot was "mentioned" in the message and proceed if it was
 
-// get the context
-// get the channel object from the message
-// fecth the previous 6 messages to use as context.
+  // get the context
+  // get the channel object from the message
+  // fecth the previous 6 messages to use as context.
+});
