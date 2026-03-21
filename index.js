cb6bd20da762e86b662dc5c8d47c34d15cec12e6
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-03-20 21:44:22 -0600

    Add new imports for command handling

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 // import dotenv and config
-import { config } from 'dotenv';
-import {fs} from 'node:fs';
+import { config } from "dotenv";
+import * as fs from "node:fs";
+import path from "node:path";
 // import Client and GatewayIntentBits from discord.js
-import {GatewayIntentBits, Client, Events, Collection} from 'discord.js';
+import {
+  GatewayIntentBits,
+  Client,
+  Events,
+  Collection,
+  MessageFlags,
+} from "discord.js";
 
 config();
 
 // discord token
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
-
 // create a new discord client instance
-const client = new Client({intents:[
-    GatewayIntentBits.Guilds, 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-]});
-
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 // create event listener for when client is ready
 // log message "Bot is online"
-client.once(Events.ClientReady, (readyClient)=>{
-    console.log(`Bot is online as ${readyClient.user.tag}`)
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Bot is online as ${readyClient.user.tag}`);
 });
 
 // have client login() using DISCORD_TOKEN
@@ -36,5 +43,5 @@ client.commands = new Collection();
 // condition to check if the bot was "mentioned" in the message and proceed if it was
 
 // get the context
-    // get the channel object from the message
-    // fecth the previous 6 messages to use as context.
+// get the channel object from the message
+// fecth the previous 6 messages to use as context.
